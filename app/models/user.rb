ae67c010719576f62a6cb7d992280f519f15f12c
sewwolf <sewwolf.gt@gmail.com>
2021-12-21 17:18:43 +0900

    エラーメッセージ修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -18,6 +18,6 @@ class User < ApplicationRecord
 
   
   PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
-validates_format_of :password, with: PASSWORD_REGEX, message: 'には英字と数字の両方を含めて設定してください'
+  validates_format_of :password, with: PASSWORD_REGEX, message: "doesn't have both letters and numbers"
 
 end
