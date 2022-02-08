0faa09c7a8fbaf4cf9d38fc2bb59b95d85200a92
sewwolf <sewwolf.gt@gmail.com>
2022-02-08 12:24:34 +0900

    エラーメッセージ　日本語化　修正

--- app/models/record_address.rb
+++ app/models/record_address.rb
@@ -1,4 +1,4 @@
-class RecordAddress
+class RecordAddress 
   include ActiveModel::Model
   attr_accessor :user_id, :item_id, :postal_code, :area_id, :municipality, :street_number, :building_name, :telephone_number,
                 :token
@@ -6,12 +6,16 @@ class RecordAddress
   with_options presence: true do
     validates :user_id
     validates :item_id
-    validates :postal_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: 'is invalid. Include hyphen(-)' }
     validates :municipality
     validates :street_number
-    validates :telephone_number, format: { with: /\A[0-9]{10,11}\z/ }
   end
-  validates :area_id, numericality: { other_than: 1, message: "can't be blank" }
+  validates :postal_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: 'はハイフン(-)を含めた正しい値で入力してください' }, allow_blank: true
+  validates :postal_code, presence: true
+
+  validates :telephone_number, format: { with: /\A[0-9]{10,11}\z/, message: 'を正しい値で入力してください'}, allow_blank: true
+  validates :telephone_number, presence: true
+
+  validates :area_id, numericality: { other_than: 1, message: "を選択してください" }, allow_blank: true
   validates :token, presence: true
   def save
     record = Record.create(user_id: user_id, item_id: item_id)
